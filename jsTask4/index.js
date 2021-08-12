fc3f7ee8ebb9f831b5dab42a301ec470a2d1cec0
SiarheiTamkovich <mpg2000@mail.ru>
2021-08-12 16:25:46 +0300

    JsTask4 - 6 tasks complete

--- jsTask4/index.js
+++ jsTask4/index.js
@@ -1,4 +1,4 @@
-//--jsTask4 Classwork Cycles While and For 
+//--jsTask4 Classwork Functions---
 
 /* --Task № 1  --
 
@@ -95,7 +95,73 @@ function factorial(n){
 let n = +prompt(`Enter the number `);
 alert(factorial(n));
 
-/* --Task № 3 collect the number  --*/
+/* ---------------------------Homework --------------------------------------
+
+/* --Task № 3 collect the number  --
+
+function collectNamber(a, b, c){
+    var r = 0;
+    r = String(a) + String(b) + String(c);
+    return r;
+}
+let a = 4;
+let b = 5;
+let c = 6;
+alert(collectNamber(a, b, c));
+
+/* --Task № 4 square area  --
+
+function areaSquare(a, b = 1){
+    var r = 0;
+    b !== 1 ? r = a * b : r = a * a;
+    return r;
+}
+let a = 4;
+//let b = 5;
+let b = undefined;
+alert(areaSquare(a, b));
+
+/* --Task № 5 perfect number  --
+
+function perfectNumber(a){
+    var r = 0;
+    for(var i = 1; i <= a/2; i++){
+ //       console.log(i);
+        a % i == 0 ? r += i : 0;
+    }
+    r == a ? alert(`Nomber is perfect`) : alert(`Nomber is imperfect`);
+}
+let a = +prompt(`Enter number`);
+perfectNumber(a);
+
+/* --Task № 6 all perfect number  --*/
+
+function perfectNumber(min, max){
+    for (min; min <= max; min++){
+        var r = 0;
+        for (var i = 1; i <= min/2; i++){
+            if (min % i == 0) { 
+                r = r + i;
+//                console.log(`min=${min} i=${i} r=${r}`);
+                r == min ? document.write(`${r} <br>`) : 0;
+            }       
+        }   
+    }
+}
+let min = +prompt(`Enter min number`);
+let max = +prompt(`Enter max number`);
+perfectNumber(min, max);
+
+/* --Task № 7   --*/
+
+
+
+
+
+
+
+
+
 
 
     
