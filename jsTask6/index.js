d082ce307f37d27ba02f69c456958a4eb9ade47f
SiarheiTamkovich <mpg2000@mail.ru>
2021-08-17 11:32:44 +0300

    classwork comllete

--- jsTask6/index.js
+++ jsTask6/index.js
@@ -1,14 +1,12 @@
 //--jsTask6 Classwork Functions---
 
 /* --Task № 1  factorial --
-
 function factorial (n){
     return n ? n * factorial (--n) : 1;
 }
 console.log(factorial(5));
 
 /* --Task № 2  vivod chisel --
-
 function poryadoknb(n, b){
     return n === b ? 1 : poryadoknb(++n, b);
 }
@@ -19,7 +17,6 @@ console.log(poryadoknb(3, 10));
 console.log(poryadokbn(3, 10));
 
 /* --Task № 3  vivod naoborot --
-
 function napered(n){
     document.write(n ? n % 10 : ``);
     if (n > 1) {
@@ -29,7 +26,6 @@ function napered(n){
 console.log(napered(21234567));
 
 /* --Task № 4  summa cifr --
-
 let result = 0;
 function summaCifr(n){
     let a = n % 10;
@@ -41,7 +37,6 @@ function summaCifr(n){
 console.log(summaCifr(21234567));
 
 /* --Task № 5  scobki --
-
 let a = `()`;
 function skobki(n){
     if (n !== 1) {
@@ -52,8 +47,7 @@ function skobki(n){
 }
 skobki(4);
 
-/* --Task № 1 object pramoygolnic --*/
-
+/* --Task № 1 object rectangle --
 let koordinaty = { 
     ax: 5,
     ay: 20,
@@ -61,12 +55,12 @@ let koordinaty = {
     by: 5
 }
 function prymougolnic(a){
-    console.log(`Верхняя левая ${a.ax}, ${a.ay} Нижняя правая ${a.bx}, ${a.by}`);
+    document.write(`Верхняя левая ${a.ax}, ${a.ay} Нижняя правая ${a.bx}, ${a.by}`);
 }
 prymougolnic(koordinaty);
+console.log(koordinaty);
 
-/* --Task № 2 object pramoygolnic --
-
+/* --Task № 2 object rectangle width --
 let koordinat = { 
     ax: 5,
     ay: 20,
@@ -74,33 +68,151 @@ let koordinat = {
     by: 5
 }
 function prymougolnic(a){
-    console.log(${a.bx - a.ax});
+    document.write(`width=${a.bx - a.ax}`);
+    console.log(koordinaty);
 }
 prymougolnic(koordinat)
 
-/* --Task № 3 object pramoygolnic --*/
-
-/* --Task № 4 object pramoygolnic ploschad --*/
-
-/* --Task № 5 object pramoygolnic perimetr --*/
-
-/* --Task № 6 object pramoygolnic izmenit shirinu --*/
-
-/* --Task № 7 object pramoygolnic izmenit visotu --
-
-let koordinat = { 
+/* --Task № 3 object rectangle hight --
+let rectangle = { 
     ax: 5,
     ay: 20,
     bx: 40,
     by: 5
 }
-function prymougolnic(a){
-    console.log(`${a.ay + h} ${a.ax + h}`);
+function extractCoordinates(a){
+    document.write(`height=${a.ay - a.ax}`);
 }
-let h = 5;
-prymougolnic(koordinat);
+extractCoordinates(rectangle)
+console.log(rectangle);
 
-/* --Task № 8 object pramoygolnic izmenit visotu --*/
+/* --Task № 4 object rectangle square --
+let rectangle = { 
+    ax: 5,
+    ay: 20,
+    bx: 40,
+    by: 5
+}
+function extCord(a){
+    document.write(`squre=${(a.bx - a.ax) * (a.ay - a.by)}`);
+}
+extCord(rectangle)
+console.log(rectangle);
+
+/* --Task № 5 object rectangle perimetr --
+let rectangle = { 
+    ax: 5,
+    ay: 20,
+    bx: 40,
+    by: 5
+}
+function extCord(a){
+    document.write(`perimeter=${(a.bx - a.ax) + (a.ay - a.by)}`);
+}
+extCord(rectangle)
+console.log(rectangle);
+
+/* --Task № 6 object rectangle chenge widh --
+let rectangle = { 
+    ax: 5,
+    ay: 20,
+    bx: 40,
+    by: 5
+}
+function echengeCord(a, b){
+    a.bx += b;
+}
+echengeCord(rectangle, 5)
+console.log(rectangle);
+
+/* --Task № 7 object rectangle chennge hight --
+let rectangle = { 
+    ax: 5,
+    ay: 20,
+    bx: 40,
+    by: 5
+}
+function echengeCord(a, b){
+    a.ay += b;
+}
+echengeCord(rectangle, 5)
+console.log(rectangle);
+
+/* --Task № 8 object rectangle chennge hight and width  --
+let rectangle = { 
+    ax: 5,
+    ay: 20,
+    bx: 40,
+    by: 5
+}
+function echengeCord(a, h, w){
+    a.ay += h;
+    a.bx += w;
+}
+echengeCord(rectangle, 5, 10)
+console.log(rectangle);
+
+* --Task № 9 object rectangle move x  --
+let rectangle = { 
+    ax: 5,
+    ay: 20,
+    bx: 40,
+    by: 5,
+}
+function echengeCord(a, m){
+    a.ax += m;
+    a.bx += m;
+}
+echengeCord(rectangle, 5,)
+console.log(rectangle);
+
+* --Task № 10 object rectangle move y  --
+let rectangle = { 
+    ax: 5,
+    ay: 20,
+    bx: 40,
+    by: 5,
+}
+function echengeCord(a, m){
+    a.ay += m;
+    a.by += m;
+}
+echengeCord(rectangle, 5);
+console.log(rectangle);
+
+* --Task № 11 object rectangle move x y  --
+let rectangle = { 
+    ax: 5,
+    ay: 20,
+    bx: 40,
+    by: 5,
+}
+function echengeCord(a, x, y){
+    a.bx += x;
+    a.ax += x;
+    a.ay += y;
+    a.by += y;
+}
+echengeCord(rectangle, 5, 10);
+console.log(rectangle);
+
+/* --Task № 12 object rectangle point coordinates  --
+let rectangle = { 
+    ax: 5,
+    ay: 20,
+    bx: 40,
+    by: 5,
+}
+function echengeCord(a, px, py){
+    px >= a.ax && px <= a.bx &&
+    py >= a.by && py <= a.ay ? 
+    console.log(`point inside rectangle`) : 
+    console.log(`point outside rectangle`);
+}
+echengeCord(rectangle, 15, 10);
+console.log(rectangle);
+
+//---------------------------Homework ---------------------------------
 
 
 
