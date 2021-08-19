c027c6f104d67027026c731a85e70e1438eb659f
SiarheiTamkovich <mpg2000@mail.ru>
2021-08-19 13:08:10 +0300

    jsTask6 objects complete

--- jsTask6/index.js
+++ jsTask6/index.js
@@ -214,18 +214,158 @@ console.log(rectangle);
 
 //---------------------------Homework ---------------------------------
 
-/* --Task № 1 object automobile --*/
-let automobile= { 
-    prod: 5,
-    ay: 20,
-    bx: 40,
-    by: 5,
+/* --Task № 1 object car --*
+let car = { 
+    prod: `Mazda`,
+    mod: 626,
+    year: 1992,
+    speed: 80,
 }
-function echengeCord(a, px, py){
-    px >= a.ax && px <= a.bx &&
-    py >= a.by && py <= a.ay ? 
-    console.log(`point inside rectangle`) : 
-    console.log(`point outside rectangle`);
+function carInfo(a){
+    document.write(`
+        Марка ${a.prod} <br>
+        Модель ${a.mod} <br>
+        Год выпуска ${a.year} <br>
+        Средняя скорость ${a.speed} <br>
+    `)
 }
-echengeCord(rectangle, 15, 10);
-console.log(rectangle);
+function timeDist(a){
+    let timeToRest = Math.floor((a / car.speed) / 4);
+    document.write(` Чтобы преодолеть расстояние в
+      ${a} км потребуется
+      ${a / car.speed + timeToRest} часов c учетом времени на отдых
+      ${timeToRest} часа
+    `)
+}
+carInfo(car); // Task 1 part 1
+console.log(car);
+timeDist(800); // Task 1 part 2
+
+/* --Task № 2 object drobi --*
+
+let drobi = {
+    chisl1: 1,
+    znam1: 2,
+    chisl2: 3,
+    znam2: 4,
+}
+function summDrobi() { // Task 2 Part 1
+    document.write(`
+     ${drobi.chisl1}/
+     ${drobi.znam1} +
+     ${drobi.chisl2}/
+     ${drobi.znam2}=
+     ${drobi.chisl1 * drobi.znam2 + drobi.chisl2 * drobi.znam1}/
+     ${Math.max(drobi.znam1, drobi.znam2)} <br>
+    `)
+}
+function raznDrobi() { // Task 2 Part 2
+    document.write(`
+     ${drobi.chisl1}/
+     ${drobi.znam1} -
+     ${drobi.chisl2}/
+     ${drobi.znam2}=
+     ${drobi.chisl1 * drobi.znam2 - drobi.chisl2 * drobi.znam1}/
+     ${Math.max(drobi.znam1, drobi.znam2)}  <br>
+    `)
+}
+function umnDrobi() { // Task 2 Part 3
+    document.write(`
+     ${drobi.chisl1}/
+     ${drobi.znam1} *
+     ${drobi.chisl2}/
+     ${drobi.znam2}=
+     ${drobi.chisl1 * drobi.chisl2}/
+     ${drobi.znam1 * drobi.znam2} <br>
+    `)
+}
+function delDrobi() { // Task 2 Part 4
+    document.write(`
+     ${drobi.chisl1}/
+     ${drobi.znam1} :
+     ${drobi.chisl2}/
+     ${drobi.znam2}=
+     ${drobi.chisl1 * drobi.znam2}/
+     ${drobi.znam1 * drobi.chisl2} <br>
+    `)
+}
+function NOD (a, b) { // наибольший общий делитель - общий знаменатель
+	if (b > a) return NOD(b, a);
+	if (!b) return a;
+	return NOD(b, a % b);
+}
+function sokrDrobi() { // Task 2 Part 5
+    document.write(`
+     ${drobi.chisl1 * drobi.znam2}/
+     ${drobi.znam1 * drobi.chisl2} =
+     ${drobi.chisl1 * drobi.znam2 / NOD (drobi.chisl1 * drobi.znam2, drobi.znam1 * drobi.chisl2)} /
+     ${drobi.znam1 * drobi.chisl2 / NOD (drobi.chisl1 * drobi.znam2, drobi.znam1 * drobi.chisl2)} <br>
+    `)
+}
+console.log(drobi)
+summDrobi() 
+raznDrobi()
+umnDrobi() 
+delDrobi()
+sokrDrobi()
+
+/* --Task № 3 object time --*/
+let time = {
+    hh: 11,
+    mm: 56,
+    ss: 23,
+}
+function dipTime() { // Task 3 part 1
+    document.write(`Time:
+     ${time.hh} hour
+     ${time.mm} min
+     ${time.ss} sec <br>
+    `)
+}
+function cheSecTime(a) { // Task 3 part 2
+    if (time.ss + a >= 60) {
+        time.mm += Math.floor((time.ss + a) / 60)
+        time.ss = (time.ss + a) - (Math.floor((time.ss + a) / 60) * 60);
+    } else {
+        time.ss += a;
+    }
+     document.write(`Change secunds=
+     ${a}: <br>
+     ${time.hh} hour
+     ${time.mm} min
+     ${time.ss} sec <br>
+    `)
+}
+function cheMinTime(a) { // Task 3 part 3
+    if (time.mm + a >= 60) {
+        time.hh += Math.floor((time.mm + a) / 60)
+ //       console.log(time.mm + a)
+ //       console.log(Math.floor((time.mm + a) / 60) * 60)
+        time.mm = (time.mm + a) - (Math.floor((time.mm + a) / 60) * 60);
+    } else {
+        time.mm += a;
+    }
+     document.write(`Change minutes=
+     ${a}: <br>
+     ${time.hh} hour
+     ${time.mm} min
+     ${time.ss} sec <br>
+    `)
+}
+function cheHouTime(a) { // Task 3 part 4
+    if (time.hh + a >= 24) {
+        time.hh = (time.hh + a) - (Math.floor((time.hh + a) / 24) * 24);
+    } else {
+        time.hh += a;
+    }
+     document.write(`Change hours=
+     ${a}: <br>
+     ${time.hh} hour
+     ${time.mm} min
+     ${time.ss} sec <br>
+    `)
+}
+dipTime()
+cheSecTime(42)
+cheMinTime(21)
+cheHouTime(16)
