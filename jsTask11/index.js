369c889897c8daba3ec52f6254372695754455eb
SiarheiTamkovich <mpg2000@mail.ru>
2021-09-14 09:17:45 +0300

    jsTask11 1 task complete

---
+++ jsTask11/index.js
@@ -0,0 +1,16 @@
+//--jsTask11 classwork Event handling ---
+
+/* --Task № 1 Random number --*/
+function random(){
+   return Math.floor(Math.random() * 100);
+}
+let btn = document.querySelector(`.btn`);
+let num = document.querySelector(`#num`)
+
+btn.addEventListener(`click`, function(){
+  num.innerHTML = random();
+});
+
+
+
+  
