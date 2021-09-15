038038992164ef4bd2b01e8b0d1e0ff53f545ed6
SiarheiTamkovich <mpg2000@mail.ru>
2021-09-15 09:42:40 +0300

    jsTask11 classwork 2 Task complete

--- jsTask11/index.js
+++ jsTask11/index.js
@@ -11,6 +11,33 @@ btn.addEventListener(`click`, function(){
   num.innerHTML = random();
 });
 
+/* --Task № 2 track mouse coordinates --*
+document.querySelector('#container').onmousemove = function (event){
+  document.querySelector('#x').innerHTML = event.offsetX;
+  document.querySelector('#y').innerHTML = event.offsetY;
+  if( event.which == 1){
+      document.querySelector('#click').innerHTML = 'Левая кнопка';
+  } else if(event.which == 3){
+      document.querySelector('#click').innerHTML = 'Правая кнопка';
+  }
+}
+
+/* --Task № 2 track mouse coordinates with addEventListener--*/
+let elem = document.querySelector(`#container`);
+let X = document.querySelector(`#x`);
+let Y = document.querySelector(`#y`);
+
+document.addEventListener(`mousemove`, function(event){
+  document.querySelector('#x').innerHTML = event.offsetX;
+  document.querySelector('#y').innerHTML = event.offsetY;
+});
+document.addEventListener(`mouseup`, function(event){
+  if (event.button == 0){
+   document.querySelector('#click').innerHTML = 'Левая кнопка';
+  } else if (event.button == 2) {
+   document.querySelector('#click').innerHTML = 'Правая кнопка'
+  }
+});
 
 
   
