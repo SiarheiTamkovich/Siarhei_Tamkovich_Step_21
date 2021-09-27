9d932ffb28f35a30eefc54cd9a00ce1507658e2c
SiarheiTamkovich <mpg2000@mail.ru>
2021-09-27 21:05:47 +0300

    feat: add classwork

--- jsTask14/index.js
+++ jsTask14/index.js
@@ -1,3 +1,14 @@
 //--jsTask14 Event listener forms ---
 
 /* --Task № 1  --*/
+
+let btn = document.querySelector('#btnSignIn');
+let login = document.querySelector('#login');
+let remember = document.querySelector('#remember');
+
+function signIn() {
+    let isChecked = remember.checked ? 'Я тебя запомнил' : 'Я тебя не запомнил';
+    alert(`Привет, ${login.value}! ${isChecked}`)
+}
+
+btn.addEventListener('click', signIn);
