1ea7289a6ac2bd8ad3100db5434b0a9b719576dc
SiarheiTamkovich <mpg2000@mail.ru>
2021-10-01 15:11:48 +0300

    feat: add task3

--- jsTask13/index.js
+++ jsTask13/index.js
@@ -41,40 +41,50 @@
 
 /* -----------------------Homework --------------------*/
 /* --Task № 1 progress bar --*/
-const progress = document.querySelector('.progress');
+// const progress = document.querySelector('.progress');
 
-progress.addEventListener('input', function() {
-  let value = this.value;
-  this.style.background = `linear-gradient(to right,
-                       #82CFD0 0%, #82CFD0 ${value}%,
-                       #fff ${value}%, white 100%)`
-})
+// progress.addEventListener('input', function() {
+//   let value = this.value;
+//   this.style.background = `linear-gradient(to right,
+//                        #82CFD0 0%, #82CFD0 ${value}%,
+//                        #fff ${value}%, white 100%)`
+// })
 
-/* --Task № 2 slider --*/
-const arrPic = [ "https://bit.ly/3ifSb8r",
-                 "https://bit.ly/3A02lzP",
-                 "https://bit.ly/3uoblOh",
-                 "https://bit.ly/3F4nmgH",
-                 "https://bit.ly/3mtnjTl",
-                ]
-pic.src = arrPic[0];
+// /* --Task № 2 slider --*/
+// const arrPic = [ "https://bit.ly/3ifSb8r",
+//                  "https://bit.ly/3A02lzP",
+//                  "https://bit.ly/3uoblOh",
+//                  "https://bit.ly/3F4nmgH",
+//                  "https://bit.ly/3mtnjTl",
+//                 ]
+// pic.src = arrPic[0];
 
-const counter = { n: 0,}
+// const counter = { n: 0,}
            
-function left(){
-  if (counter.n === 4) counter.n = 0; 
-  counter.n++;
-  pic.src = arrPic[counter.n];
-}
+// function left(){
+//   if (counter.n === 4) counter.n = 0; 
+//   counter.n++;
+//   pic.src = arrPic[counter.n];
+// }
            
-function right(){
-  if (counter.n === 0) counter.n = 5; 
-  counter.n--;
-  pic.src = arrPic[counter.n];
-  console.log(counter.n)
-}
+// function right(){
+//   if (counter.n === 0) counter.n = 5; 
+//   counter.n--;
+//   pic.src = arrPic[counter.n];
+//   console.log(counter.n)
+// }
 
 /* --Task № 3 text blocks --*/
+let isHidden 
 textblock.addEventListener(`click`, e => {
-  console.log(e.target.parentNode.childNodes[3])
+  let isHidden = e.target.parentNode.childNodes[3].classList.value;
+  if (isHidden) {
+    e.target.parentNode.childNodes[3].classList.remove(`hidden`);
+  } else {
+    e.target.parentNode.childNodes[3].classList.add(`hidden`);
+  }
 })
+
+/* --Task № 4  blocks --*/
+
+
