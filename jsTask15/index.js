345b7f03942336a79845ebfa60e172259b64304c
SiarheiTamkovich <mpg2000@mail.ru>
2021-10-04 21:08:47 +0300

    feat: add functiolnal

--- jsTask15/index.js
+++ jsTask15/index.js
@@ -1,11 +1,44 @@
 //--jsTask15 Todo list ---
-const arrTip = [{text: `test text`, checked: `yes`,}];
+const tipContainer = document.querySelector(`.tipContainer`);
 
-
-function addItem(){
-
+class TodoTip {
+    constructor(id, text, isChecked, isDeleted){
+        this.id = id;
+        this.text = text;
+        this.isChecked = isChecked;
+        this.isDeleted = isDeleted;
+    }
 }
 
-console.log(tiptxt.value)
-console.log()
+const bdToDoList = [];
+addItem(new TodoTip (`tip001`, `Buy a new car`, false, false));
+addItem(new TodoTip (`tip002`, `Write a program "ToDoList"`, false, false));
+//console.log(bdToDoList)
+
+loadNotes();
+
+function loadNotes(){
+    for (let i = 0; i < bdToDoList.length; i++){
+        ${bdToDoList[i].text}
+    }
+}
+function addItem(tip){
+    if (tip) {
+        bdToDoList.push(tip);
+    } else { 
+        bdToDoList.push(new TodoTip(`tip001`, `${newTipTxt.value}`, false, false));
+        loadNotes();
+    }
+}
+
+function addItemInList(item){
+    let tip = document.createElement(`div`);
+    tip.className = `tip-item`;
+    tip.innerHTML = `<input class="item-check" type="checkbox">
+    <p class="tip-item_text"></p>
+    <button class="item-btn_edit">&#10000;</button>
+    <button class="item-btn_delete">&#10006;</button>`
+    tipContainer.appendChild(item);
+}
+
 
