eaaa9d7c695485479684efb9532f5ddafd62b056
SiarheiTamkovich <mpg2000@mail.ru>
2021-10-11 21:36:53 +0300

    feat: add  classes

--- jsTask15/index.js
+++ jsTask15/index.js
@@ -1,6 +1,18 @@
 //--jsTask15 Todo list ---
-const tipContainer = document.querySelector(`.tipContainer`);
 
+// create Data Base of notes
+class BdToDoList {
+    constructor (bdTips) {
+        this.bdTips = bdTips;
+    }
+    addItem(item){
+        this.bdTips.push(item);
+    }
+
+}
+const bdToDoList = new BdToDoList([]);
+
+// create object of tips
 class TodoTip {
     constructor(id, text, isChecked, isDeleted){
         this.id = id;
@@ -10,35 +22,54 @@ class TodoTip {
     }
 }
 
-const bdToDoList = [];
-addItem(new TodoTip (`tip001`, `Buy a new car`, false, false));
-addItem(new TodoTip (`tip002`, `Write a program "ToDoList"`, false, false));
-//console.log(bdToDoList)
 
-loadNotes();
+bdToDoList.addItem(new TodoTip (`tip00`, `Buy a new car`, false, false));
+bdToDoList.addItem(new TodoTip (`tip00`, `Write a program "ToDoList"`, false, false));
 
-function loadNotes(){
-    for (let i = 0; i < bdToDoList.length; i++){
-        ${bdToDoList[i].text}
+const tipContainer = document.querySelector(`.tipContainer`);
+
+loadNotesToDocument(bdToDoList);
+
+function loadNotesToDocument(bdToDoList){ 
+    for (let i = 0; i < bdToDoList.bdTips.length; i++){
+        let tip = document.createElement(`div`);
+        tipContainer.appendChild(tip);
+        tip.className = `tip-item`;
+        tip.id = `tip${i}`
+        tip.innerHTML = `<input class="item-check" type="checkbox" id="ch${i}" onclick="checkItem()">
+        <input class="tip-item_text" type="text" value="${bdToDoList.bdTips[i].text}" readonly id="txt${i}" onkeypress="saveEditText()">
+        <button class="item-btn_edit" id="btnedit${i}" onclick="editItem()">&#10000;</button>
+        <button class="item-btn_delete" id="btndelete${i}">&#10006;</button>`; 
     }
 }
-function addItem(tip){
-    if (tip) {
-        bdToDoList.push(tip);
-    } else { 
-        bdToDoList.push(new TodoTip(`tip001`, `${newTipTxt.value}`, false, false));
-        loadNotes();
+
+function addNewItem() { 
+    bdToDoList.addItem(new TodoTip (`tip00`, newTipTxt.value, false, false));
+    delDom();
+    loadNotesToDocument(bdToDoList);
+}
+
+function delDom() {
+    while (tipContainer.firstChild) {
+        tipContainer.removeChild(tipContainer.firstChild);
     }
 }
 
-function addItemInList(item){
-    let tip = document.createElement(`div`);
-    tip.className = `tip-item`;
-    tip.innerHTML = `<input class="item-check" type="checkbox">
-    <p class="tip-item_text"></p>
-    <button class="item-btn_edit">&#10000;</button>
-    <button class="item-btn_delete">&#10006;</button>`
-    tipContainer.appendChild(item);
+function checkItem() {
+ //   console.log(event.target.parentNode.childNodes[2])
+    event.target.parentNode.childNodes[2].classList.toggle("crossed");
+    
 }
+
+function editItem() {
+  //  console.log(event.target.parentNode.childNodes);
+    event.target.parentNode.childNodes[2].removeAttribute(`readonly`);
+    event.target.parentNode.childNodes[2].classList.toggle("edit");
+}
+
+function saveEditText() {
+    event.target.parentNode.childNodes[2].classList.toggle("edit");
+}
+
 
 
