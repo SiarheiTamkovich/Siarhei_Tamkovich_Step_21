defb5603d7eea60aed4626a55065cb520b2d5dc8
SiarheiTamkovich <mpg2000@mail.ru>
2021-10-12 16:57:08 +0300

    feat: add classes

--- jsTask15/index.js
+++ jsTask15/index.js
@@ -2,10 +2,11 @@
 
 // create Data Base of notes
 class BdToDoList {
+    
     constructor (bdTips) {
         this.bdTips = bdTips;
     }
-    addItem(item){
+    addItem(item){     
         this.bdTips.push(item);
     }
 
@@ -22,54 +23,62 @@ class TodoTip {
     }
 }
 
-
+// some tips for tests
 bdToDoList.addItem(new TodoTip (`tip00`, `Buy a new car`, false, false));
 bdToDoList.addItem(new TodoTip (`tip00`, `Write a program "ToDoList"`, false, false));
 
-const tipContainer = document.querySelector(`.tipContainer`);
-
-loadNotesToDocument(bdToDoList);
-
-function loadNotesToDocument(bdToDoList){ 
-    for (let i = 0; i < bdToDoList.bdTips.length; i++){
-        let tip = document.createElement(`div`);
-        tipContainer.appendChild(tip);
-        tip.className = `tip-item`;
-        tip.id = `tip${i}`
-        tip.innerHTML = `<input class="item-check" type="checkbox" id="ch${i}" onclick="checkItem()">
-        <input class="tip-item_text" type="text" value="${bdToDoList.bdTips[i].text}" readonly id="txt${i}" onkeypress="saveEditText()">
-        <button class="item-btn_edit" id="btnedit${i}" onclick="editItem()">&#10000;</button>
-        <button class="item-btn_delete" id="btndelete${i}">&#10006;</button>`; 
-    }
-}
-
-function addNewItem() { 
-    bdToDoList.addItem(new TodoTip (`tip00`, newTipTxt.value, false, false));
-    delDom();
-    loadNotesToDocument(bdToDoList);
-}
-
-function delDom() {
-    while (tipContainer.firstChild) {
-        tipContainer.removeChild(tipContainer.firstChild);
-    }
-}
-
-function checkItem() {
- //   console.log(event.target.parentNode.childNodes[2])
-    event.target.parentNode.childNodes[2].classList.toggle("crossed");
+// The main interface block
+class InterfaceToDo {
     
+    constructor (tipContainer){
+        this.tipContainer = tipContainer;
+    }
+    
+    loadNotesToDocument(bdToDoList){ 
+        for (let i = 0; i < bdToDoList.bdTips.length; i++){
+            let tip = document.createElement(`div`);
+            this.tipContainer.appendChild(tip);
+            tip.className = `tip-item`;
+            tip.id = `tip${i}`
+            tip.innerHTML = `<input class="item-check" type="checkbox" id="ch${i}" onclick="interfaceToDo.checkItem()">
+            <input class="tip-item_text" type="text" value="${bdToDoList.bdTips[i].text}" readonly id="txt${i}" onkeypress="saveEditText()">
+            <button class="item-btn_edit" id="btnedit${i}" onclick="editItem()">&#10000;</button>
+            <button class="item-btn_delete" id="btndelete${i}">&#10006;</button>`; 
+        }
+    }
+
+    addNewItem() { 
+       bdToDoList.addItem(new TodoTip (`tip00`, newTipTxt.value, false, false)); // add tips to data base
+       while (this.tipContainer.firstChild) { // clear container
+        this.tipContainer.removeChild(this.tipContainer.firstChild); 
+        }
+       this.loadNotesToDocument(bdToDoList); // load data to container
+    }
+
+    checkItem() {
+    //   console.log(event.target.parentNode.childNodes[2])
+        event.target.parentNode.childNodes[2].classList.toggle("crossed");
+        
+    }
+
+    editItem() {
+    //  console.log(event.target.parentNode.childNodes);
+        event.target.parentNode.childNodes[2].removeAttribute(`readonly`);
+        event.target.parentNode.childNodes[2].classList.toggle("edit");
+    }
+
+    saveEditText() {
+        event.target.parentNode.childNodes[2].classList.toggle("edit");
+    }
 }
 
-function editItem() {
-  //  console.log(event.target.parentNode.childNodes);
-    event.target.parentNode.childNodes[2].removeAttribute(`readonly`);
-    event.target.parentNode.childNodes[2].classList.toggle("edit");
-}
+// define of Div container
+const tipContainer = document.querySelector(`.tipContainer`);
+interfaceToDo = new InterfaceToDo(tipContainer); // create interface and start
 
-function saveEditText() {
-    event.target.parentNode.childNodes[2].classList.toggle("edit");
-}
+interfaceToDo.loadNotesToDocument(bdToDoList); // load notes
+
+
 
 
 
