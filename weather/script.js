f28a3407da239afb7b8559c58d6844182620438c
SiarheiTamkovich <mpg2000@mail.ru>
2021-10-17 21:46:07 +0300

    init: add task wether

---
+++ weather/script.js
@@ -0,0 +1,25 @@
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+const weatherDescription = document.querySelector('.weather-description');
+const city = document.querySelector('.city');
+
+async function getWeather() {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.textContent}&lang=ru&appid=dc39d1fe382c37ae2ad0e7d140d117ab&units=metric`;
+  const res = await fetch(url);
+  const data = await res.json();
+  
+  weatherIcon.className = 'weather-icon owf';
+  weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+  temperature.textContent = `${data.main.temp.toFixed(0)}°C`;
+  weatherDescription.textContent = data.weather[0].description;
+}
+
+function setCity(event) {
+  if (event.code === 'Enter') {
+    getWeather();
+    city.blur();
+  }
+}
+
+document.addEventListener('DOMContentLoaded', getWeather);
+city.addEventListener('keypress', setCity);
