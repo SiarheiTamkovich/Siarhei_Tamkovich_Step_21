3d8c292c1ba2f005755871dc66cf72c71d7e456b
SiarheiTamkovich <mpg2000@mail.ru>
2021-10-18 22:07:12 +0300

    feat: add link to movie

--- moviesearch/script.js
+++ moviesearch/script.js
@@ -1,11 +1,10 @@
-
 const movie = document.querySelector('.movie');
-
-async function getWeather() {
-  const url = `http://www.omdbapi.com/`;
+async function getMovie() {
+  const url = `http://www.omdbapi.com/?i=tt3896198&apikey=6cb20a41`;
   const res = await fetch(url);
   const data = await res.json();
+  console.log(data)
 }
 
-getWeather();
+getMovie();
    
