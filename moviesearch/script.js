c1cc606b3061585d5b94640c3444da81047daf6f
SiarheiTamkovich <mpg2000@mail.ru>
2021-10-20 01:22:51 +0300

    feat: add link

--- moviesearch/script.js
+++ moviesearch/script.js
@@ -1,6 +1,6 @@
 const movie = document.querySelector('.movie');
 async function getMovie() {
-  const url = `http://www.omdbapi.com/?i=tt3896198&apikey=6cb20a41`;
+  const url = `http://www.omdbapi.com/?s=mesto*&plot=full&page=1&type=movie&apikey=6cb20a41`;
   const res = await fetch(url);
   const data = await res.json();
   console.log(data)
