be0ce7bf5e1c63e44c53e1dd1775a2cfa1753535
SiarheiTamkovich <mpg2000@mail.ru>
2021-10-25 21:50:19 +0300

    feat: add details of film

--- moviesearch/script.js
+++ moviesearch/script.js
@@ -1,12 +1,28 @@
 const movie = document.querySelector('.movie');
+const film = document.querySelector(`.film`);
+let objMovie = getLocalStorage();
+
+//console.log(objMovie.Response)
 
 async function getMovie() {
-  const url = `http://www.omdbapi.com/?s=gum*&plot=full&page=1&type=movie&apikey=6cb20a41`;
+  complete.innerText = `Loading...`
+  const url = `http://www.omdbapi.com/?s=${searchtxt.value}*&plot=full&page=1&type=${select.value}&apikey=6cb20a41`;
  // const url = `http://www.omdbapi.com/?i=tt0760170&plot=short&apikey=6cb20a41`
   const res = await fetch(url);
   const data = await res.json();
-  complete.innerText = `Loading...`
-  setLocalStorage(data);
+ 
+  if (data.Response === `True`) {
+    setLocalStorage(data);
+    console.log(data.Response)
+    complete.innerText = `Найдено: ${data.totalResults} записей`;
+    film.innerHTML = ``;
+    loadMovieToDom(data)
+  } else if (data.Response === 'False'){
+    complete.innerText = `Ошибка: ${data.Error}`;
+    film.innerHTML = ``;
+    console.log(data)
+  }
+  
 }
 
 function setLocalStorage(data) {
@@ -23,22 +39,29 @@ function getLocalStorage() {
 }
 window.addEventListener('load', getLocalStorage);
 
-let objMovie = getLocalStorage();
-console.log(objMovie)
-complete.innerText = `Найдено: ${objMovie.totalResults} записей`;
+loadMovieToDom(objMovie);
 
-const film = document.querySelector(`.film`);
+function loadMovieToDom(data) {
 
-for (let i = 0; i < objMovie.Search.length; i++) {
-  let newDiv = document.createElement("div");
-      newDiv.classList.toggle(`film_item`)
-      newDiv.innerHTML = `
-      <p class="item_type"> ${objMovie.Search[i].Type}</p>
-      <h2 class="item_title">${objMovie.Search[i].Title}</h2>
-      <h3 class="item_year">${objMovie.Search[i].Year}</h3>
-      <div class="item_poster"><img src="${objMovie.Search[i].Poster}" class="poster_pict"></div>
-      <button class="item_btn">Details</button`;
-      film.appendChild(newDiv);
-} 
+  for (let i = 0; i < data.Search.length; i++) {
+    let newDiv = document.createElement("div");
+        newDiv.classList.toggle(`film_item`)
+        newDiv.id = data.Search[i].imdbID;
+        newDiv.innerHTML = `
+        <p class="item_type"> ${data.Search[i].Type}</p>
+        <h2 class="item_title">${data.Search[i].Title}</h2>
+        <h3 class="item_year">${data.Search[i].Year}</h3>
+        <div class="item_poster"><img src="${data.Search[i].Poster}" class="poster_pict"></div>
+        <button class="item_btn" onclick="openText()">Details</button`;
+        film.appendChild(newDiv);
+  } 
+}
 
-    
+function openText() {
+  console.log(event.target.parentNode.id)
+  event.target.parentNode.innerHTML = `
+  <frame class="frame">rrtytyyty</frame>
+  <button class="frame_btn" onclick="closeframe()">Return</button> `;
+  
+
+}
