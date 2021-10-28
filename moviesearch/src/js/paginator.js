fdfecfc621a9ccfbfef2b1ecae3a599af6e04c84
SiarheiTamkovich <mpg2000@mail.ru>
2021-10-28 13:05:14 +0300

    feat: add paginator arrow

--- moviesearch/src/js/paginator.js
+++ moviesearch/src/js/paginator.js
@@ -3,7 +3,9 @@ const divPaginator = document.querySelector(`.paginator`)
 function addButtons(numberbtn, page) {
     let total = Math.ceil(numberbtn / page);
     divPaginator.insertAdjacentHTML('afterbegin',
-    `<button>1 str.</button>
+    `<button class="paginator-btn_left">&#9668;&#9668;</button>
+     <h3> &emsp; page 1 from 300 &emsp; <h3>
+     <button class="paginator-btn_right">&#9658;&#9658;</button>
     `);
     console.log(total)
 }
