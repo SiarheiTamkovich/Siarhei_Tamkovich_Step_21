42ca9346cb91c04f4572b4373d719987f8f292e6
SiarheiTamkovich <mpg2000@mail.ru>
2021-12-01 14:56:25 +0300

    refuctor: adit a comments

--- jsTaskNew/index.js
+++ jsTaskNew/index.js
@@ -3,26 +3,26 @@
 // of 3 or 5 below the number passed in. If the number is negative, 
 //return 0.
 
-// function calcSum(prop) {
-//   let sum = 0;
-//   if (prop <= 0) {
-//     console.log(`the number is negative or = 0`)
-//   } else { 
-//     for (let i = 1; i <= prop; i++){
-//       if (i % 3 === 0 || i % 5 === 0) {
-//         sum += i;
-//       }
-//     }
-//     console.log(`the number ${prop} sum of all the multiples of 3 or 5 = ${sum}`)
-//   }
-// }
+function calcSum(prop) {
+  let sum = 0;
+  if (prop <= 0) {
+    console.log(`the number is negative or = 0`)
+  } else { 
+    for (let i = 1; i <= prop; i++){
+      if (i % 3 === 0 || i % 5 === 0) {
+        sum += i;
+      }
+    }
+    console.log(`the number ${prop} sum of all the multiples of 3 or 5 = ${sum}`)
+  }
+}
 
-// calcSum(0);
-// calcSum(-5);
-// calcSum(3);
-// calcSum(5);
-// calcSum(9);
-// calcSum(21);
+calcSum(0);
+calcSum(-5);
+calcSum(3);
+calcSum(5);
+calcSum(9);
+calcSum(21);
 
 //Task 2
 //Given n, return the sum of its digits. If the resulting value is 
